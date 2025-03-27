aa05ba1ae0a70efae67f67ba10faa3325581c3de
Jihan Yao 姚季涵 <61811791+yaojh18@users.noreply.github.com>
2025-03-27 13:16:36 -0700

    Revert "image gen merged"

--- utils.py
+++ utils.py
@@ -1,6 +1,4 @@
 import openai
-from google import genai
-from google.genai import types
 import time
 import base64
 import collections
@@ -13,7 +11,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import torch
 import torch.nn.functional as F
-from urllib.request import urlopen
 from tqdm import tqdm
 from PIL import Image
 from io import BytesIO
@@ -22,17 +19,12 @@ from torchvision import transforms
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from skimage.metrics import structural_similarity as ssim
 from sklearn.metrics import cohen_kappa_score
-from transformers import AutoTokenizer, AutoModel, AutoImageProcessor, AutoModelForCausalLM, AutoProcessor, ClapModel, AutoModelForSpeechSeq2Seq, Wav2Vec2FeatureExtractor, WavLMForXVector
-from transformers.generation.configuration_utils import GenerationConfig
-from transformers.generation import LogitsProcessorList, PrefixConstrainedLogitsProcessor
+from transformers import AutoProcessor, ClapModel, AutoModelForSpeechSeq2Seq, Wav2Vec2FeatureExtractor, WavLMForXVector
 
 
 OPENAI_KEY = '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'
 GEMINI_KEY = 'AIzaSyB-MKMN8fRHpk6LLLR9jrkJfeUxLzX70s8'
-# HF_KEY = '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'
-HF_KEY = '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' # yyj's hf token
-REPLICATE_KEY = 'r8_UK8hAuFDdTWdUVsHNtHAov6TaBDo8Vw1zph3t'
-
+HF_KEY = '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'
 IMAGE_TOKEN = lambda x: f'<image_start><image_{x}><image_end>'
 AUDIO_TOKEN = lambda x: f'<audio_start><audio_{x}><audio_end>'
 FAILED_TOKEN = '<none>'
