e7b70536421d6dd941976a1e7de87aa841b87c46
BeanHam <binhan96816@gmail.com>
2025-04-02 16:56:06 -0700

    removed bash files; mulfunction

--- env_setups/tango2.sh
+++
@@ -1,5 +0,0 @@
-## set up tango2 environments
-conda create -n tango python=3.10 -y && conda activate tango
-git clone https://github.com/declare-lab/tango.git
-cd tango
-pip install -r requirements.txt
